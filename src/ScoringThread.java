65e207d0df767afc41aebd53c0d43530895a041f
J-Barta <53406552+J-Barta@users.noreply.github.com>
2022-02-17 09:17:12 -0500

    Revert "Revert "Merge branch 'master' into absurdle""
    This reverts commit e18ea8ec27d5a0119342134d0b80dcaf6709491c.

--- src/ScoringThread.java
+++ src/ScoringThread.java
@@ -10,18 +10,23 @@ public class ScoringThread extends Thread{
     int id;
 
     List<WordData> wordsWithData = new ArrayList<>();
-     private boolean finished = false;
+
+    private boolean finished = false;
+
+    WordScoring scoring;
+
     public ScoringThread(List<String> toSort, List<String> fullList, int id) {
         super();
         this.toSort = toSort;
         this.fullList = fullList;
         this.id = id;
+        scoring = new WordScoring();
     }
 
     @Override
     public void run() {
         try {
-            wordsWithData = WordScoring.sortWordList(toSort, fullList, id);
+            wordsWithData = scoring.sortWordList(toSort, fullList, id);
             finished = true;
         }
         catch (Exception e) {
@@ -31,6 +36,10 @@ public class ScoringThread extends Thread{
 
     public boolean isFinished() {
         return finished;
+    }
+
+    public double wordsSearched() {
+        return scoring.totalSearched;
     }
 
     public List<WordData> getSortedList() {
